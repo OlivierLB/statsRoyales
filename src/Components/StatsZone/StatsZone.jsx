3748280b72b89059114a0b4ed918ba6adb61ae43
OlivierLB <lebloas.olivier@gmail.com>
2020-01-12 17:40:40 +0100

    barre de progression

--- src/Components/StatsZone/StatsZone.jsx
+++ src/Components/StatsZone/StatsZone.jsx
@@ -1,16 +1,34 @@
 import React, {Component} from 'react';
-import {Container} from "react-grid-system";
+import {Container, Row, Col} from "react-grid-system";
 export default class StatsZone extends Component {
 
     render() {
         return(
             <React.Fragment>
                 <div className="statsZone">
-                    aa
+                    <Container>
+                        <Row>
+                            <Col sm={2}>
+                                One of three columns
+                            </Col>
+                            <Col sm={8}>
+                                <div className="-progress-meter">
+                                    <progress max="100" value="80">80</progress>
+                                    <progress max="100" value="20">20</progress>
+                                    <progress max="100" value="48">48</progress>
+                                    <progress max="100" value="21">21</progress>
+                                    <progress max="100" value="64">64</progress>
+                                </div>
+                            </Col>
+                            <Col sm={2}>
+                                One of three columns
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
                 <div className="navBottom">
                     <ul>
-                        <li>Global</li>
+                        <li className="-active">Global</li>
                         <li>Solo</li>
                         <li>Duo</li>
                         <li>Squad</li>
